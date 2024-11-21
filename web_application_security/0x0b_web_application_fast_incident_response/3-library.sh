a04b0e4c1e45a2daf8f482d0bf3c95d47fb43c2e
JoseMoreno00 <accgitjosem@gmail.com>
2024-11-21 18:34:41 -0500

    Task 3 |  Bash script to find out which tool or library the attacker used by analyzing the User-Agent strings.

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -oP "\"\-\" \"[[:print:]]+" logs.txt | sort | uniq -c | sort -n | tail -n 1 | grep -oP "[a-zA-Z][[:print:]]+" | head -c -6 | awk '{print $0 "\n"}' 
+grep -oP "\"\-\" \"[[:print:]]+" logs.txt | sort | uniq -c | sort -n | tail -n 1 | head -c -6 | grep -oP "[a-zA-Z][[:print:]]+"

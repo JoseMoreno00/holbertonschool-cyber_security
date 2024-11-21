ccb9ffead32f326b7ecf2bf02681aaa6085e810f
JoseMoreno00 <accgitjosem@gmail.com>
2024-11-20 23:03:01 -0500

    Task 0 |  Bash script to identify the IP address responsible for the most requests in a log file

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -oP "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" logs.txt | sort | uniq -c | sort -n | tail -n 1
+sort logs.txt | uniq -c | sort -n | tail -n 1 | grep -oP "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+"

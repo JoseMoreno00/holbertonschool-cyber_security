d9edef2ed2563d4fd708e0705af98eadba2d0032
JoseMoreno00 <accgitjosem@gmail.com>
2025-01-24 14:01:53 -0500

    task 6

---
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,8 @@
+require 'net/http'
+def get_request(url)
+        uri = URI(url)
+        res = Net::HTTP.get_response(uri)
+
+        puts "Response status: #{res.code} #{res.message}"
+        puts "Response body:\n#{res.body}"
+end

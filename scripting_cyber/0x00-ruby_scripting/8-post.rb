6bd6da79b3359acc2930567a3b180ff53a345259
JoseMoreno00 <accgitjosem@gmail.com>
2025-01-25 16:41:27 -0300

    Task 8

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,9 @@
+require 'net/http'
+
+def post_request(url, body_params)
+    uri = URI(url)
+    res = Net::HTTP.post_form(uri, body_params)
+
+    puts "Response status: #{res.code} #{res.message}"
+    puts "Response body:\n#{res.body}"
+end
